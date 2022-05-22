7579b555e424ed5fa4422c994b7e9e1cf94657be
Iulian Mirica <iulianmirica@microsoft.com>
2022-05-22 16:36:25 +0300

    .

--- Algorithms/Searching/LinearSearch.py
+++ Algorithms/Searching/LinearSearch.py
@@ -1,1 +1,23 @@
-#LinearSearch
+# If x is present then return its location
+# else return -1
+ 
+ 
+def search(arr, n, x):
+ 
+    for i in range(0, n):
+        if (arr[i] == x):
+            return i
+    return -1
+ 
+ 
+# Driver Code
+arr = [2, 3, 4, 10, 40]
+x = 10
+n = len(arr)
+ 
+# Function call
+result = search(arr, n, x)
+if(result == -1):
+    print("Element is not present in array")
+else:
+    print("Element is present at index", result)
