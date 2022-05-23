87736905d346e54ca2c1a5473ccd2903faac6fa7
imirica <88032703+imirica@users.noreply.github.com>
2022-05-23 20:37:55 +0300

    Update SimpleSum.py

--- Problems/Arrays/SimpleSum.py
+++ Problems/Arrays/SimpleSum.py
@@ -1,10 +1,10 @@
 #Display the sum of all the elements of a given array
 
-def arraySum(arr):
-    arrsum=0
+def array_sum(arr):
+    arr_sum=0
     for i in range(len(arr)):
-        arrsum+=arr[i]
-    return arrsum
+        arr_sum+=arr[i]
+    return arr_sum
 
-#print(arraySum([1,2,3,4,5]))
+#print(array_sum([1,2,3,4,5]))
 #
