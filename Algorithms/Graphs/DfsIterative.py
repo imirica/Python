24f412e7ed1aea5b9ca9c764700d8add8c2c61d7
imirica <88032703+imirica@users.noreply.github.com>
2022-05-30 18:54:28 +0300

    Update DfsIterative.py

--- Algorithms/Graphs/DfsIterative.py
+++ Algorithms/Graphs/DfsIterative.py
@@ -3,16 +3,21 @@
 def dfs(graph, source):
 
     visited = set()
+    
     if source not in graph:
         print("Node not in the graph")
         return
+    
     stack=[]
     stack.append(source)
+    
     while stack:
         current = stack.pop()
+        
         if current not in visited:
             print(current)
             visited.add(current)
+            
             for neighbor in graph[current]:
                 stack.append(neighbor)
 
