8ebb2d486dfa408f4393a29b26a63416491ba336
imirica <88032703+imirica@users.noreply.github.com>
2022-06-02 13:45:41 +0300

    Update AreAnagramsCounter.py

--- Problems/Strings/AreAnagramsCounter.py
+++ Problems/Strings/AreAnagramsCounter.py
@@ -6,3 +6,10 @@ def are_anagrams(s1,s2):
     if len(s1)!=len(s2):
         return False
     return Counter(s1)==Counter(s2)
+
+"""
+def are_anagrams(s1,s2):
+    if len(s1)!=len(s2):
+        return False
+    return sorted(s1)==sorted(s2)
+"""
