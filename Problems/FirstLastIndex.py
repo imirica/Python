20b810269544912607b82df14f575ffdddd6f3e5
imirica <88032703+imirica@users.noreply.github.com>
2022-06-02 17:36:30 +0300

    Update FirstLastIndex.py

--- Problems/FirstLastIndex.py
+++ Problems/FirstLastIndex.py
@@ -1,6 +1,8 @@
 """
 Given a sorted array of integers arr and an integer target, find the index of the first and the last position of target in arr.
 If target can't be found return [-1,-1]
+
+both solutions are O(n)
 """
 
 def first_last(arr,target):
