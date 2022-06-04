d441342f1d34e1abcfac8ffe6125179b400b4b8f
imirica <88032703+imirica@users.noreply.github.com>
2022-06-04 19:02:51 +0300

    Update and rename IeWebDriver to IeWebDriver.py

--- WorkRelated/IeWebDriver.py
+++ WorkRelated/IeWebDriver.py
@@ -8,5 +8,4 @@ ie_options.add_additional_option("ie.edgechromium", True)
 ie_options.add_additional_option("ie.edgepath",'C:\Program Files (x86)\Microsoft\Edge\Application\msedge.exe')
 
 driver = webdriver.Ie('C:\\IEDriverServer\\IEDriverServer.exe',options=ie_options)
-
 driver.get("http://www.bing.com")
