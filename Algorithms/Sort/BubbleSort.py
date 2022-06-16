4a32cb8a8d92f1af2f0ff97d9cb9ffbcc8c0a27e
imirica <88032703+imirica@users.noreply.github.com>
2022-06-16 13:34:56 +0300

    Update BubbleSort.py

--- Algorithms/Sort/BubbleSort.py
+++ Algorithms/Sort/BubbleSort.py
@@ -1,7 +1,7 @@
 #Bubble Sort
 
 def bubble_sort(arr):
-    for i in range(len(arr)-1):
+    for i in range(len(arr)-1,0,-1):
         for j in range(i):
             if arr[j]>arr[j+1]:
                 arr[j],arr[j+1]=arr[j+1],arr[j]
