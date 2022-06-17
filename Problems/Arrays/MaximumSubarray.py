29c1a4b5a0a6be80a28741ae21bfb4c2cc3a790d
imirica <88032703+imirica@users.noreply.github.com>
2022-06-18 01:09:30 +0300

    Create MaximumSubarray.py

---
+++ Problems/Arrays/MaximumSubarray.py
@@ -0,0 +1,25 @@
+"""
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+A subarray is a contiguous part of an array.
+"""
+
+from xmlrpc.client import MININT
+
+
+def max_subarr(arr):
+
+    #maxSum and the actual sum take minimum possible integer value
+    suma=maxim=MININT
+    for i in range(len(arr)):
+        for j in range(i,len(arr)):
+            suma=sum(arr[i:j+1])
+            if suma>maxim:
+                maxim=suma
+    return maxim
+
+"""
+arr=[1,2,3,-10,9,-11,8,]
+
+print(max_subarr(arr))
+"""
