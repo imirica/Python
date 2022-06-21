91aaf10b4dd40d9227351375e51ed93c63fa1021
imirica <88032703+imirica@users.noreply.github.com>
2022-06-21 16:18:26 +0300

    Update TwoSum.py

--- Problems/HashTable/TwoSum.py
+++ Problems/HashTable/TwoSum.py
@@ -9,3 +9,22 @@ Input: nums = [2,7,11,15], target = 9
 Output: [0,1]
 Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
 """
+
+def two_sum(arr, target):
+
+    complement_dict=dict()
+
+    for i in range(len(arr)):
+        complement=target-arr[i]
+        if arr[i] in complement_dict:
+            return complement_dict[arr[i]],i
+        else:
+            complement_dict[complement]=i
+            print(complement_dict)
+            
+   """
+arr=[2,7,11,15]
+target=17 
+
+print(two_sum(arr, target))
+   """
