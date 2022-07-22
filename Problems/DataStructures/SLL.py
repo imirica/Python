d6e51365d0f74e852c7b29e24218a0fb319e0997
imirica <88032703+imirica@users.noreply.github.com>
2022-07-22 18:29:12 +0300

    Create SLL.py

---
+++ Problems/DataStructures/SLL.py
@@ -0,0 +1,11 @@
+#implement a single linked list
+
+class Node:
+    def __init__(self, value=None):
+        self.value = value
+        self.next = None
+
+class SLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
