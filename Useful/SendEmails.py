2b0897926ab10fa18e1205a3c2e3f301b07cfacf
imirica <88032703+imirica@users.noreply.github.com>
2023-06-07 13:29:21 +0300

    Update SendEmails.py

--- Useful/SendEmails.py
+++ Useful/SendEmails.py
@@ -1,1 +1,19 @@
-#text
+import os
+import smtplib
+from email.message import EmailMessage
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+contacts = ['email1', 'email2']
+
+msg = EmailMessage()
+msg['Subject'] = 'Check out Bronx as a puppy!'
+msg['From'] = EMAIL_ADDRESS
+msg['To'] = contacts
+
+msg.set_content('This is a plain text email_v3')
+
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.send_message(msg)
