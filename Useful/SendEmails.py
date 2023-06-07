657d3504c7dc7d76c109df74920b8eb940faf63e
imirica <88032703+imirica@users.noreply.github.com>
2023-06-07 13:42:45 +0300

    Update SendEmails.py

--- Useful/SendEmails.py
+++ Useful/SendEmails.py
@@ -14,6 +14,16 @@ msg['To'] = contacts
 
 msg.set_content('This is just a test email V1')
 
+""" 
+#add an attachment to the email : 
+with open('file', 'rb') as f:
+    file_data = f.read()
+    file_name = f.name
+
+msg.add_attachment(file_data, maintype = 'application', subtype = 'octet-stream', filename = file_name)
+
+"""
+
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
     smtp.send_message(msg)
