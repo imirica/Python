840816d2964d28bf292a2b04bc25e2365732f5fe
imirica <88032703+imirica@users.noreply.github.com>
2023-07-30 22:38:09 +0300

    Update LeetCode0035.py

--- LeetCode/LeetCode0035.py
+++ LeetCode/LeetCode0035.py
@@ -2,11 +2,23 @@
 
 You must write an algorithm with O(log n) runtime complexity."""
 
-def binary_search(arr, start, end, target):
-
+def find_insert_position(arr, start, end, target):
+    # Calculate the mid index of the current range
     mid = (start + end) // 2
 
-    if start > end : return start
-    elif arr[mid] == target: return mid
-    elif arr[mid] > target: return binary_search(arr, start, mid - 1, target)
-    else: return binary_search(arr, mid + 1, end, target)
+    # Base case: If the start index exceeds the end index, it means the target is not found.
+    if start > end:
+        # In this case, return the start index, which represents the position where the target should be inserted.
+        return start
+
+    # If the middle element matches the target, return its index.
+    elif arr[mid] == target:
+        return mid
+
+    # If the middle element is greater than the target, search in the left half of the array.
+    elif arr[mid] > target:
+        return find_insert_position(arr, start, mid - 1, target)
+
+    # If the middle element is smaller than the target, search in the right half of the array.
+    else:
+        return find_insert_position(arr, mid + 1, end, target)
