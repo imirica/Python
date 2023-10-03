3cccefa47906afd6b85e91f6163ff02aafd69786
imirica <88032703+imirica@users.noreply.github.com>
2023-10-03 18:11:22 +0300

    Create LeetCode0118.py

---
+++ LeetCode/LeetCode0118.py
@@ -0,0 +1,19 @@
+def generate(numRows):
+    if numRows == 0:
+        return []
+    
+    # Initialize the result with the first row
+    result = [[1]]
+    
+    for i in range(1, numRows):
+        # Generate the current row based on the previous row
+        prev_row = result[-1]
+        current_row = [1]
+        for j in range(1, len(prev_row)):
+            current_row.append(prev_row[j - 1] + prev_row[j])
+        current_row.append(1)
+        
+        # Append the current row to the result
+        result.append(current_row)
+    
+    return result
